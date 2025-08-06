85fb9beb9eb8d0a1ca51dea77ab4309c5cc3ee6d
LeganSar44 <hoangslevan@gmail.com>
2025-08-06 16:35:01 +0700

    update structure project

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,6 +26,7 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/api/test', require('./routes/testRoutes'));
 
 // Health check route
 app.get('/api/health', (req, res) => {
